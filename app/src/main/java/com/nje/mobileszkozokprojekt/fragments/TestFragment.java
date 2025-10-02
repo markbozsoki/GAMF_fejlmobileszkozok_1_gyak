2f3cc5a1725195687bbf4ad4a22635a57463ac72
norbertkovacsgit <k.norbi093@gmail.com>
2025-10-02 18:56:49 +0200

    creating_test_fragment

---
+++ app/src/main/java/com/nje/mobileszkozokprojekt/fragments/TestFragment.java
@@ -0,0 +1,21 @@
+package com.nje.mobileszkozokprojekt.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nje.mobileszkozokprojekt.R;
+
+public class TestFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_test, container, false);
+    }
+}
