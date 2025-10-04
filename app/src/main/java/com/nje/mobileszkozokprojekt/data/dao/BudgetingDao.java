4f722c383b27c0ad5d86cfe6a86ffb12edfd3921
markbozsoki <bozsoki.mark@icloud.com>
2025-10-04 20:35:43 +0200

    budgeting_dao_body

--- app/src/main/java/com/nje/mobileszkozokprojekt/data/dao/BudgetingDao.java
+++ app/src/main/java/com/nje/mobileszkozokprojekt/data/dao/BudgetingDao.java
@@ -1,4 +1,27 @@
 package com.nje.mobileszkozokprojekt.data.dao;
 
+import com.nje.mobileszkozokprojekt.data.entity.BudgetingEntity;
+
+import java.util.List;
+
 public interface BudgetingDao {
+
+    List<BudgetingEntity> getAll();
+
+    BudgetingEntity getById(String id);
+
+    /**
+     * Returns true on success, returns false on error
+     */
+    boolean insert(BudgetingEntity entity);
+
+    /**
+     * Returns true on success, returns false on error
+     */
+    boolean update(BudgetingEntity entity);
+
+    /**
+     * Returns true on success, returns false on error
+     */
+    boolean delete(BudgetingEntity entity);
 }
