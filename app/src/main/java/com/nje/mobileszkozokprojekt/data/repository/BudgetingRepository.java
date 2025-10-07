1c0abfbd18abc9c05af6a548ea04ce664adabcbd
markbozsoki <bozsoki.mark@icloud.com>
2025-10-07 19:03:59 +0200

    repository_query_implementation

--- app/src/main/java/com/nje/mobileszkozokprojekt/data/repository/BudgetingRepository.java
+++ app/src/main/java/com/nje/mobileszkozokprojekt/data/repository/BudgetingRepository.java
@@ -1,5 +1,6 @@
 package com.nje.mobileszkozokprojekt.data.repository;
 
+import com.nje.mobileszkozokprojekt.data.FinDatabase;
 import com.nje.mobileszkozokprojekt.data.entity.BudgetingEntity;
 import com.nje.mobileszkozokprojekt.data.repository.interfaces.IRepository;
 
@@ -8,28 +9,57 @@ import java.util.List;
 
 public class BudgetingRepository implements IRepository<BudgetingEntity> {
 
+    private final FinDatabase database;
+
+    public BudgetingRepository(FinDatabase database) {
+        this.database = database;
+    }
+
     @Override
     public List<BudgetingEntity> getAll() {
-        return Collections.emptyList();
+        try {
+            return database.budgetingDao().getAll();
+        } catch (Exception ex) {
+            return Collections.emptyList();
+        }
     }
 
     @Override
     public BudgetingEntity getById(String id) {
-        return null;
+        try {
+            return database.budgetingDao().getById(id);
+        } catch (Exception ex) {
+            return null;
+        }
     }
 
     @Override
     public boolean insert(BudgetingEntity entity) {
-        return false;
+        try {
+            database.budgetingDao().insert(entity);
+        } catch (Exception ex) {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public boolean update(BudgetingEntity entity) {
-        return false;
+        try {
+            database.budgetingDao().update(entity);
+        } catch (Exception ex) {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public boolean delete(BudgetingEntity entity) {
-        return false;
+        try {
+            database.budgetingDao().delete(entity);
+        } catch (Exception ex) {
+            return false;
+        }
+        return true;
     }
 }
