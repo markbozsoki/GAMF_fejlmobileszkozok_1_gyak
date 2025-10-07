224ccf73e0ba5735af395b68280f61e5782f4566
markbozsoki <bozsoki.mark@icloud.com>
2025-10-07 19:02:35 +0200

    budgeting_dao

--- app/src/main/java/com/nje/mobileszkozokprojekt/data/dao/BudgetingDao.java
+++ app/src/main/java/com/nje/mobileszkozokprojekt/data/dao/BudgetingDao.java
@@ -1,12 +1,16 @@
 package com.nje.mobileszkozokprojekt.data.dao;
 
+import androidx.room.Query;
+
 import com.nje.mobileszkozokprojekt.data.entity.BudgetingEntity;
 
 import java.util.List;
 
 public interface BudgetingDao extends DefaultDao<BudgetingEntity> {
 
+    @Query("SELECT * FROM budgeting")
     List<BudgetingEntity> getAll();
 
+    @Query("SELECT * FROM budgeting WHERE id = :id")
     BudgetingEntity getById(String id);
 }
--- app/src/main/java/com/nje/mobileszkozokprojekt/data/entity/BudgetingEntity.java
+++ app/src/main/java/com/nje/mobileszkozokprojekt/data/entity/BudgetingEntity.java
@@ -1,16 +1,29 @@
 package com.nje.mobileszkozokprojekt.data.entity;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import com.nje.mobileszkozokprojekt.data.entity.interfaces.IEntityWithId;
 
+@Entity(tableName = "budgeting")
 public class BudgetingEntity implements IEntityWithId {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    public BudgetingEntity(
+            int id
+    ) {
+        this.id = id;
+    }
 
     @Override
     public int getId() {
-        return 0;
+        return id;
     }
 
     @Override
     public void setId(int id) {
-
+        this.id = id;
     }
 }
