7b2732db53f2bfe857f0c6b9a20ada847a0d6ef3
norbertkovacsgit <k.norbi093@gmail.com>
2025-10-07 20:57:20 +0200

    creating_the_estimation_fragment

---
+++ app/src/main/java/com/nje/mobileszkozokprojekt/fragment/EstimationFragment.java
@@ -0,0 +1,19 @@
+package com.nje.mobileszkozokprojekt.fragment;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nje.mobileszkozokprojekt.R;
+
+public class EstimationFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_estimation, container, false);
+    }
+}
