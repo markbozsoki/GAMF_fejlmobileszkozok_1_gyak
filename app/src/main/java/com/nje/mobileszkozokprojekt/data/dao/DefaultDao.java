2e0bbaebe998b413e48c5a49a3229792663b6971
markbozsoki <bozsoki.mark@icloud.com>
2025-10-07 19:02:00 +0200

    default_dao_room_annotations

--- app/src/main/java/com/nje/mobileszkozokprojekt/data/dao/DefaultDao.java
+++ app/src/main/java/com/nje/mobileszkozokprojekt/data/dao/DefaultDao.java
@@ -1,19 +1,28 @@
 package com.nje.mobileszkozokprojekt.data.dao;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Update;
+
+@Dao
 public interface DefaultDao<T> {
 
     /**
      * Returns true on success, returns false on error
      */
+    @Insert
     boolean insert(T entity);
 
     /**
      * Returns true on success, returns false on error
      */
+    @Update
     boolean update(T entity);
 
     /**
      * Returns true on success, returns false on error
      */
+    @Delete
     boolean delete(T entity);
 }
