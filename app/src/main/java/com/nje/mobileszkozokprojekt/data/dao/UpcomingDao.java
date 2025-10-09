95fe1bfc25dae93024d7900964fc6602c06dad92
MoSzabina <moszabina@gmail.com>
2025-10-09 11:26:23 +0200

    add_upcoming_dao

--- app/src/main/java/com/nje/mobileszkozokprojekt/data/dao/UpcomingDao.java
+++ app/src/main/java/com/nje/mobileszkozokprojekt/data/dao/UpcomingDao.java
@@ -1,12 +1,18 @@
 package com.nje.mobileszkozokprojekt.data.dao;
 
+import androidx.room.Dao;
+import androidx.room.Query;
+
 import com.nje.mobileszkozokprojekt.data.entity.UpcomingEntity;
 
 import java.util.List;
 
+@Dao
 public interface UpcomingDao extends DefaultDao<UpcomingEntity> {
 
+    @Query("SELECT * FROM upcoming")
     List<UpcomingEntity> getAll();
 
+    @Query("SELECT * FROM upcoming WHERE id = :id")
     UpcomingEntity getById(String id);
 }
