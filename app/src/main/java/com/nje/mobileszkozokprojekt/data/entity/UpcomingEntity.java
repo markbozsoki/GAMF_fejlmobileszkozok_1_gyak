2d57f14f408a3601c5717de85e837a166511d284
MoSzabina <moszabina@gmail.com>
2025-10-09 11:24:01 +0200

    add_upcoming_entity

--- app/src/main/java/com/nje/mobileszkozokprojekt/data/entity/UpcomingEntity.java
+++ app/src/main/java/com/nje/mobileszkozokprojekt/data/entity/UpcomingEntity.java
@@ -1,4 +1,59 @@
 package com.nje.mobileszkozokprojekt.data.entity;
 
-public class UpcomingEntity {
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import com.nje.mobileszkozokprojekt.data.entity.interfaces.IEntityWithId;
+
+@Entity(tableName = "upcoming")
+public class UpcomingEntity implements IEntityWithId {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String name;
+    private String type;
+    private String category;
+    private double value; // in USD
+    private String dueDate;
+
+    public UpcomingEntity(
+            int id,
+            String name,
+            String type,
+            String category,
+            double value,
+            String dueDate
+    ) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.category = category;
+        this.value = value;
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+
+    public double getValue() { return value; }
+    public void setValue(double value) { this.value = value; }
+
+    public String getDueDate() { return dueDate; }
+    public void setDueDate(String dueDate) { this.dueDate = dueDate; }
 }
