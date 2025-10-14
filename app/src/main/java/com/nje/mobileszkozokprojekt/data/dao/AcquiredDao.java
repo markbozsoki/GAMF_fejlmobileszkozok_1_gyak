2b7b02869d6ae900d24f3b7ce9977c74bd3bfcd6
norbertkovacsgit <k.norbi093@gmail.com>
2025-10-14 12:27:52 +0200

    base_sql_queries

--- app/src/main/java/com/nje/mobileszkozokprojekt/data/dao/AcquiredDao.java
+++ app/src/main/java/com/nje/mobileszkozokprojekt/data/dao/AcquiredDao.java
@@ -1,12 +1,17 @@
 package com.nje.mobileszkozokprojekt.data.dao;
 
+import androidx.room.Dao;
+import androidx.room.Query;
+
 import com.nje.mobileszkozokprojekt.data.entity.AcquiredEntity;
 
 import java.util.List;
-
+@Dao
 public interface AcquiredDao extends DefaultDao<AcquiredEntity> {
 
+    @Query("SELECT * FROM acquired")
     List<AcquiredEntity> getAll();
 
+    @Query("SELECT * FROM acquired WHERE id = :id")
     AcquiredEntity getById(String id);
 }
