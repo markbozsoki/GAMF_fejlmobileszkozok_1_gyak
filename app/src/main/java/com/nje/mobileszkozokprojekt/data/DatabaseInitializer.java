ded7c6a4ae2fee93ce059320bd0972a6a03ab08a
markbozsoki <bozsoki.mark@icloud.com>
2025-10-25 14:23:10 +0200

    db_fixup

--- app/src/main/java/com/nje/mobileszkozokprojekt/data/DatabaseInitializer.java
+++ app/src/main/java/com/nje/mobileszkozokprojekt/data/DatabaseInitializer.java
@@ -12,6 +12,10 @@ public class DatabaseInitializer {
         this.dataBase = dataBase;
     }
 
+    public void clearDatabase() {
+        dataBase.databaseDao().clearAllItemsFromDatabase();
+    }
+
     public void populateDatabase() {
         seedBudgetingEntities();
         seedUpcomingEntities();
@@ -23,13 +27,15 @@ public class DatabaseInitializer {
         dataBase.budgetingDao().insert(new BudgetingEntity(2, "grocery", "outgoing", "cost", 200));
         dataBase.budgetingDao().insert(new BudgetingEntity(3, "car payment", "outgoing", "debt", 135.48));
         dataBase.budgetingDao().insert(new BudgetingEntity(4, "EFT investing", "outgoing", "investment", 50));
+        dataBase.budgetingDao().insert(new BudgetingEntity(6, "concert ticket", "outgoing", "cost", 15));
+        dataBase.budgetingDao().insert(new BudgetingEntity(8, "stock investing", "outgoing", "investment", 50));
     }
 
     private void seedUpcomingEntities(){
         dataBase.upcomingDao().insert(new UpcomingEntity(1, "tax return", "incoming", 407, "2026-05-01"));
         dataBase.upcomingDao().insert(new UpcomingEntity(2, "insurance", "outgoing", 160, "2026-10-12"));
         dataBase.upcomingDao().insert(new UpcomingEntity(3, "EFT profit", "incoming", 11.02, "2025-12-31"));
-        dataBase.upcomingDao().insert(new UpcomingEntity(2, "property tax", "outgoing", 80.53, "2026-04-30"));
+        dataBase.upcomingDao().insert(new UpcomingEntity(4, "property tax", "outgoing", 80.53, "2026-04-30"));
     }
 
     private void seedAcquiredEntities(){
