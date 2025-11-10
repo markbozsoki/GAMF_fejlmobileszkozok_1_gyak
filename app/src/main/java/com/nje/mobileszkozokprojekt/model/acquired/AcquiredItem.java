3457f9b14de7f2247109c102fe98de1d655bce58
norbertkovacsgit <k.norbi093@gmail.com>
2025-11-10 10:34:09 +0100

    item_values_for_acquired_page

--- app/src/main/java/com/nje/mobileszkozokprojekt/model/acquired/AcquiredItem.java
+++ app/src/main/java/com/nje/mobileszkozokprojekt/model/acquired/AcquiredItem.java
@@ -1,4 +1,28 @@
 package com.nje.mobileszkozokprojekt.model.acquired;
 
-public class AcquiredHelperModel {
+public class AcquiredItem {
+
+    private String name;
+    private CategoryProvider type;
+    private CategoryProvider category;
+    private double value;
+
+    public AcquiredItem(String name, CategoryProvider type, CategoryProvider category, double value) {
+        this.name = name;
+        this.type = type;
+        this.category = category;
+        this.value = value;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public CategoryProvider getType() { return type; }
+    public void setType(CategoryProvider type) { this.type = type; }
+
+    public CategoryProvider getCategory() { return category; }
+    public void setCategory(CategoryProvider category) { this.category = category; }
+
+    public double getValue() { return value; }
+    public void setValue(double value) { this.value = value; }
 }
