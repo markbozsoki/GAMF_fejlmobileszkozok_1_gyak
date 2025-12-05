4322d96081cf881950ee6e2f2f85b5c1c30e49d7
norbertkovacsgit <k.norbi093@gmail.com>
2025-12-05 17:16:45 +0100

    extend acquiredItem with id

--- app/src/main/java/com/nje/mobileszkozokprojekt/model/acquired/AcquiredItem.java
+++ app/src/main/java/com/nje/mobileszkozokprojekt/model/acquired/AcquiredItem.java
@@ -2,17 +2,22 @@ package com.nje.mobileszkozokprojekt.model.acquired;
 
 public class AcquiredItem {
 
+    private int id;
     private String name;
     private CategoryProvider type;
     private CategoryProvider category;
     private double value;
 
-    public AcquiredItem(String name, CategoryProvider type, CategoryProvider category, double value) {
+    public AcquiredItem(int id, String name, CategoryProvider type, CategoryProvider category, double value) {
+        this.id = id;
         this.name = name;
         this.type = type;
         this.category = category;
         this.value = value;
     }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
