2133bca956e783e8cadffc89fb1a792a6b82208a
markbozsoki <bozsoki.mark@icloud.com>
2025-12-09 22:16:34 +0100

    add_adapter_and_holder

---
+++ app/src/main/java/com/nje/mobileszkozokprojekt/fragment/EstimationViewAdapter.java
@@ -0,0 +1,26 @@
+package com.nje.mobileszkozokprojekt.fragment;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class EstimationViewAdapter extends RecyclerView.Adapter<EstimationViewHolder> {
+
+
+    @NonNull
+    @Override
+    public EstimationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EstimationViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/nje/mobileszkozokprojekt/fragment/EstimationViewHolder.java
@@ -0,0 +1,12 @@
+package com.nje.mobileszkozokprojekt.fragment;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class EstimationViewHolder extends RecyclerView.ViewHolder {
+    public EstimationViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
