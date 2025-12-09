f83020a2af1059b58ef771503a1d4aa444209a6a
markbozsoki <bozsoki.mark@icloud.com>
2025-12-09 20:48:37 +0100

    inject_repositories

--- app/src/main/java/com/nje/mobileszkozokprojekt/fragment/EstimationFragment.java
+++ app/src/main/java/com/nje/mobileszkozokprojekt/fragment/EstimationFragment.java
@@ -7,8 +7,23 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.nje.mobileszkozokprojekt.R;
+import com.nje.mobileszkozokprojekt.data.entity.AcquiredEntity;
+import com.nje.mobileszkozokprojekt.data.entity.BudgetingEntity;
+import com.nje.mobileszkozokprojekt.data.entity.UpcomingEntity;
+import com.nje.mobileszkozokprojekt.data.repository.interfaces.IRepository;
+
+import javax.inject.Inject;
 
 public class EstimationFragment extends Fragment {
+
+    @Inject
+    IRepository<AcquiredEntity> acquiredRepository;
+
+    @Inject
+    IRepository<BudgetingEntity> budgetingRepository;
+
+    @Inject
+    IRepository<UpcomingEntity> upcomingRepository;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
