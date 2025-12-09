c6ac23589af9ce1cec6f3f4b397c3e3b36337c36
markbozsoki <bozsoki.mark@icloud.com>
2025-12-09 22:10:59 +0100

    add_view_elements

--- app/src/main/java/com/nje/mobileszkozokprojekt/fragment/EstimationFragment.java
+++ app/src/main/java/com/nje/mobileszkozokprojekt/fragment/EstimationFragment.java
@@ -2,10 +2,14 @@ package com.nje.mobileszkozokprojekt.fragment;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
 import com.nje.mobileszkozokprojekt.R;
 import com.nje.mobileszkozokprojekt.data.entity.AcquiredEntity;
 import com.nje.mobileszkozokprojekt.data.entity.BudgetingEntity;
@@ -26,8 +30,14 @@ public class EstimationFragment extends Fragment {
     IRepository<UpcomingEntity> upcomingRepository;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(
+            LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        View view = inflater.inflate(R.layout.fragment_estimation, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.estimationListRecyclerView);
+        BarChart diagram = view.findViewById(R.id.estimationDiagram);
 
         return inflater.inflate(R.layout.fragment_estimation, container, false);
     }
