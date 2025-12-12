e7e49d78b002a0ef205a94ad5a4d912e0d28f10f
markbozsoki <bozsoki.mark@icloud.com>
2025-12-12 01:33:15 +0100

    implement_rec_view

--- app/src/main/java/com/nje/mobileszkozokprojekt/fragment/EstimationViewHolder.java
+++ app/src/main/java/com/nje/mobileszkozokprojekt/fragment/EstimationViewHolder.java
@@ -1,12 +1,20 @@
 package com.nje.mobileszkozokprojekt.fragment;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.nje.mobileszkozokprojekt.R;
+
 public class EstimationViewHolder extends RecyclerView.ViewHolder {
+
+    public TextView valueTextView;
+    public TextView dateTextView;
     public EstimationViewHolder(@NonNull View itemView) {
         super(itemView);
+        valueTextView = itemView.findViewById(R.id.estimationValueTextView);
+        dateTextView = itemView.findViewById(R.id.estimationDateTextView);
     }
 }
